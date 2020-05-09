876fcda2fc128a29808d7c8564d542ef84e83eaf
arnab-shanta-anu <arnabshantaanu@gmail.com>
2020-05-10 05:46:15 +0600

    getting msg body via click

--- fb-secure-msg.js
+++ fb-secure-msg.js
@@ -1,1 +1,13 @@
-document.body.style.border = "5px solid red";
+//document.body.textContent = "";
+
+//var header = document.createElement('h1');
+//header.textContent = "This page has been eaten";
+//document.body.appendChild(header);
+
+let x = document.querySelector('[data-text="true"]');
+x.addEventListener('click', print);
+x.addEventListener('keyup',print);
+
+function print(){
+    console.log(x.innerText);
+}
