1dcf9fe074364c1b6ab7d561874ff70f5a518cf3
arnab-shanta-anu <arnabshantaanu@gmail.com>
2020-05-18 04:05:23 +0600

    global eventsnoop

--- fb-secure-msg.js
+++ fb-secure-msg.js
@@ -1,8 +1,12 @@
+window.addEventListener('keyup',print,true);
+document.addEventListener('keyup',print,true);
+
 
 let x = document.querySelector('[data-text="true"]');
 x.setAttribute("contenteditable","true");
 x.onkeyup = print();
 
-function print(){
+function print(e){
     console.log(x.innerHTML);
+    console.log(e.keycode);
 }
