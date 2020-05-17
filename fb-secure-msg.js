b49a94f9f51e7f25ef237c14eb1a7d8aa6825971
arnab-shanta-anu <arnabshantaanu@gmail.com>
2020-05-18 04:36:50 +0600

    on the track

--- fb-secure-msg.js
+++ fb-secure-msg.js
@@ -1,12 +1,22 @@
 window.addEventListener('keyup',print,true);
-document.addEventListener('keyup',print,true);
+//document.addEventListener('keyup',print,true);
 
-
+var inputs = [];
 let x = document.querySelector('[data-text="true"]');
 x.setAttribute("contenteditable","true");
 x.onkeyup = print();
 
 function print(e){
-    console.log(x.innerHTML);
-    console.log(e.keycode);
+    //console.log(x.innerHTML);
+    console.log(e.key);
+    if(e.key !== "Backspace"||
+        e.key !== "Delete")
+        inputs.push(e.key);
+    console.log(inputs);
 }
+function receiveMsg(msg, sender, sendResponse){
+    console.log(msg.message);
+
+}
+chrome.runtime.onMessage.addListener(receiveMsg);
+
