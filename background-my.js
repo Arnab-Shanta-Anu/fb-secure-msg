2c5a350ca726dbf8808864acfbd1004598be8e8a
arnab-shanta-anu <arnabshantaanu@gmail.com>
2020-05-18 05:37:05 +0600

    formatting

--- background-my.js
+++ background-my.js
@@ -1,10 +1,14 @@
-function sayHello(tab){
-    if(tab.url.indexOf('facebook.com/messages') >-1 ){
-        //console.log("match");
-        let msg = {
-            message: "send me message to encrypt"
-        }
-        chrome.tabs.sendMessage(tab.id, msg);
-    }
-};
+function sayHello(tab) {
+  if (tab.url.indexOf("facebook.com/messages") > -1) {
+    //console.log("match");
+    let msg = {
+      message: "send me message to encrypt",
+    };
+    var port = chrome.runtime.connect({ name: "fb-sec-msg" });
+    port.postMessage(msg);
+    port.onMessage.addListener(function (msg) {
+      console.log(msg.message);
+    });
+  }
+}
 chrome.browserAction.onClicked.addListener(sayHello);
--- fb-secure-msg.js
+++ fb-secure-msg.js
@@ -1,22 +1,29 @@
-window.addEventListener('keyup',print,true);
-//document.addEventListener('keyup',print,true);
+// window.addEventListener('keyup',print,true);
+// document.addEventListener('keyup',print,true);
 
-var inputs = [];
-let x = document.querySelector('[data-text="true"]');
-x.setAttribute("contenteditable","true");
-x.onkeyup = print();
+// var inputs = [];
+// let x = document.querySelector('[data-text="true"]');
+// x.setAttribute("contenteditable","true");
+// x.onkeyup = print();
 
-function print(e){
-    //console.log(x.innerHTML);
-    console.log(e.key);
-    if(e.key !== "Backspace"||
-        e.key !== "Delete")
-        inputs.push(e.key);
-    console.log(inputs);
-}
-function receiveMsg(msg, sender, sendResponse){
+// function print(e){
+//     console.log(x.innerHTML);
+//     console.log(e.key||"nothing");
+//     if(e.key !== "Backspace"||
+//         e.key !== "Delete")
+//         inputs.push(e.key);
+//     console.log(inputs);
+// }
+//function receiveMsg(msg, sender, sendResponse){
+//    console.log(msg.message);
+//}
+//chrome.runtime.onMessage.addListener(receiveMsg);
+
+chrome.runtime.onConnect.addListener(function (port) {
+  console.assert(port.name == "fb-sec-msg");
+  port.onMessage.addListener(function (msg) {
     console.log(msg.message);
-
-}
-chrome.runtime.onMessage.addListener(receiveMsg);
-
+    port.postMessage({ message: "hello ag" });
+    return true;
+  });
+});
